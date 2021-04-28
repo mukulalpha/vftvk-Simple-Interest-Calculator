6d0854a01c80721394c46b15eb1ce4868cf4eba2
Mukul Attavania <39954201+mukulalpha@users.noreply.github.com>
2021-04-28 15:45:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years)
+    result = document.getElementById("result");
+    result.innerHTML=
+    "If you deposite" + " " + (principal) + " <br> " + 
+    "At an interest rate of" + " " + (rate) + " <br> "+ 
+    "You will recieve an amount of" + " " + (principal * years * rate / 100) + " <br>" + 
+    "In the year" + " " + (year);
 
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
 
-    function updateRate() 
-{
+}
+    
+function updateRate() {
+
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+        
+function validation() {
+
+    var num = /^[0]+$/;
+    if (principal.value.match(num))  {
+
+    alert("Enter Positive Number");
+
+    
+
+    }
+    else {
+
+        null;
+    }
 
 }
+
         
